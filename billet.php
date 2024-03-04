07650f0803ceb813dfea1d683eb11080ae3d3077
Ndeye Mbombe Cisse <ndeyecisse188@gmail.com>
2024-03-04 16:52:38 +0000

    mien place de la connexion

---
+++ billet.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Application de reservation</title>
+</head>
+<body>
+<header>
+    <div>
+        <img src="cuisine.png" alt="">
+    </div>
+    <nav>
+        <a href="creation.php">ADD BILLET</a>
+        <a href="pays.php">LIST_BILLET</a>
+        
+
+    </nav>
+  </header> 
+</body>
+</html>
